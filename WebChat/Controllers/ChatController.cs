dee8b95e02d9bfdf7f198e1dfd853fb31903e984
Andrew Romanuk <blek213@gmail.com>
2017-07-07 22:24:58 +0300

    A new master project

---
+++ WebChat/Controllers/ChatController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebChat.Models;
+
+namespace WebChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Room
+        [HttpGet]
+        public ActionResult Room()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Room(string val)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebChat/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebChat.Models;
+using System.Net;
+using System.Net.Mail;
+
+namespace WebChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserContext DbUser = new UserContext();
+        // GET: Home
+        [HttpGet]
+        public ActionResult LoginForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginForm(string login, string password)
+        {
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult RegisterForm()
+        {
+            return View(DbUser.Users);
+        }
+        [HttpPost]
+        public ActionResult RegisterForm(string nickname, string email, string password)
+        {
+            
+            
+            return View();
+        }
+        
+
+    }
+}
